e1b4b0c33646376e23ddd8d154a159c77acf6de5
pentekmate <erno801@gmail.com>
2026-02-26 16:48:58 +0100

    taskresource rework

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -14,113 +14,124 @@ class TaskResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        if ($this->task_type->name === 'short_answer') {
-            return [
-                'task_title' => $this->task_title,
-                'task_description' => $this->task_description,
-                'task_type' => 'shortAnswer',
-                'feedback' => $this->task_shortAnswer?->feedback,
+     return match ($this->task_type->name) {
+        'short_answer' => $this->shortAnswerData(),
+        'grouping' => $this->groupingData(),
+        'assignment' => $this->assignmentData(),
+        'pairing' => $this->pairingData(),
+        default => [],
+    };
 
-                'questionsOrImages' => $this->task_shortAnswer?->questions->map(function ($question) {
+    }
+
+    private function shortAnswerData()
+    {
+        return [
+            'task_title' => $this->task_title,
+            'task_description' => $this->task_description,
+            'task_type' => 'shortAnswer',
+            'feedback' => $this->task_shortAnswer?->feedback,
+
+            'questionsOrImages' => $this->task_shortAnswer?->questions->map(function ($question) {
+                return [
+                    'id' => $question->id,
+                    'question' => $question->question,
+                    'img' => $question->imgURL,
+                ];
+            }),
+
+            'answers' => $this->task_shortAnswer?->questions
+                ->map(fn ($q) => $q->answer?->answer)
+                ->filter()
+                ->values(),
+        ];
+    }
+
+    private function groupingData()
+    {
+        return [
+            'task_title' => $this->task_title,
+            'task_description' => $this->task_description,
+            'task_type' => 'grouping',
+            'feedback' => $this->task_grouping?->feedback,
+
+            'groups' => $this->task_grouping?->groups
+                ->map(function ($group) {
+                    return [
+                        'id' => $group->id,
+                        'name' => $group->name,
+                    ];
+                })->values(),
+
+            'group_items' => $this->task_grouping?->groups
+                ->flatMap(function ($group) {
+                    return $group->items->map(function ($item) {
+                        return [
+                            'id' => $item->id,
+                            'name' => $item->name,
+
+                        ];
+                    });
+                })->values(),
+        ];
+    }
+
+    private function assigmentData()
+    {
+        $image = $this->task_assignment?->image;
+
+        return [
+            'task_title' => $this->task_title,
+            'task_description' => $this->task_description,
+            'task_type' => 'assignment',
+
+            'img' => $image?->imageURL, // vagy ami az oszlop neve
+
+            'coordinates' => $image?->assignmentCoordinates
+                ->map(function ($coordinate) {
+                    return [
+                        'id' => $coordinate->id,
+                        'coordinate' => $coordinate->coordinate,
+                    ];
+                })->values(),
+
+            'answers' => $image?->assignmentCoordinates
+                ->flatMap(function ($coordinate) {
+                    return $coordinate->assignmentAnswers->map(function ($answer) {
+                        return [
+                            'answer' => $answer->answer,
+
+                        ];
+                    });
+                }),
+        ];
+    }
+
+    private function pairingData()
+    {
+        return [
+            'task_title' => $this->task_title,
+            'task_description' => $this->task_description,
+            'task_type' => 'pairing',
+            'feedback' => $this->task_pair?->feedback,
+
+            'pairQuestions' => $this->task_pair?->pairGroups
+                ->flatMap->questions
+                ->map(function ($question) {
                     return [
                         'id' => $question->id,
                         'question' => $question->question,
                         'img' => $question->imgURL,
                     ];
                 }),
-
-                'answers' => $this->task_shortAnswer?->questions
-                    ->map(fn ($q) => $q->answer?->answer)
-                    ->filter()
-                    ->values(),
-            ];
-        }
-        if ($this->task_type->name === 'grouping') {
-
-            return [
-                'task_title' => $this->task_title,
-                'task_description' => $this->task_description,
-                'task_type' => 'grouping',
-                'feedback' => $this->task_grouping?->feedback,
-
-                'groups' => $this->task_grouping?->groups
-                    ->map(function ($group) {
-                        return [
-                            'id' => $group->id,
-                            'name' => $group->name,
-                        ];
-                    })->values(),
-
-                'group_items' => $this->task_grouping?->groups
-                    ->flatMap(function ($group) {
-                        return $group->items->map(function ($item) {
-                            return [
-                                'id' => $item->id,
-                                'name' => $item->name,
-
-                            ];
-                        });
-                    })->values(),
-            ];
-        }
-        if ($this->task_type->name === 'assignment') {
-
-            $image = $this->task_assignment?->image;
-
-            return [
-                'task_title' => $this->task_title,
-                'task_description' => $this->task_description,
-                'task_type' => 'assignment',
-
-                'img' => $image?->imageURL, // vagy ami az oszlop neve
-
-                'coordinates' => $image?->assignmentCoordinates
-                    ->map(function ($coordinate) {
-                        return [
-                            'id' => $coordinate->id,
-                            'coordinate' => $coordinate->coordinate,
-                        ];
-                    })->values(),
-
-                'answers' => $image?->assignmentCoordinates
-                    ->flatMap(function ($coordinate) {
-                        return $coordinate->assignmentAnswers->map(function ($answer) {
-                            return [
-                                'answer' => $answer->answer,
-                                
-                            ];
-                        });
-                    }),
-            ];
-        }
-
-        if ($this->task_type->name === 'pairing') {
-            return [
-                'task_title' => $this->task_title,
-                'task_description' => $this->task_description,
-                'task_type' => 'pairing',
-                'feedback' => $this->task_pair?->feedback,
-
-                'pairQuestions' => $this->task_pair?->pairGroups
-                    ->flatMap->questions
-                    ->map(function ($question) {
-                        return [
-                            'id' => $question->id,
-                            'question' => $question->question,
-                            'img' => $question->imgURL,
-                        ];
-                    }),
-                'pairAnswers' => $this->task_pair?->pairGroups->flatMap->answers
-                    ->map(function ($answer) {
-                        return [
-                            'id' => $answer->id,
-                            'answer' => $answer->answer,
-                            'img' => $answer->imgURL,
-                        ];
-                    }),
-            ];
-        }
-
-        return [];
+            'pairAnswers' => $this->task_pair?->pairGroups->flatMap->answers
+                ->map(function ($answer) {
+                    return [
+                        'id' => $answer->id,
+                        'answer' => $answer->answer,
+                        'img' => $answer->imgURL,
+                    ];
+                }),
+        ];
     }
 }
