26a05c180c48fc12abf2cae2ddf01cc796465f7f
pentekmate <erno801@gmail.com>
2026-02-27 13:40:09 +0100

    worksheet with grouping json

--- app/Http/Requests/StoreWorksheetRequest.php
+++ app/Http/Requests/StoreWorksheetRequest.php
@@ -71,11 +71,12 @@ class StoreWorksheetRequest extends FormRequest
 
     private function validatePairing($validator, $task, $index)
     {
-      if (count($task['pairing']['pairing_groups'] ?? []) > 8) {
+        if (count($task['pairing']['pairing_groups'] ?? []) > 8) {
             $validator->errors()->add(
                 "tasks.$index.pairing.pairing_groups",
-                "Maximum 8 párt alkothatsz."
+                'Maximum 8 párt alkothatsz.'
             );
+
             return;
         }
 
@@ -90,26 +91,91 @@ class StoreWorksheetRequest extends FormRequest
 
         foreach ($task['pairing']['pairing_groups'] as $gIndex => $group) {
 
-            if (empty($group['pair_question'])) {
+            $this->validateStringField(
+                $validator,
+                $group['pair_question'] ?? null,
+                "tasks.$index.pairing.pairing_groups.$gIndex.pair_question",
+                'A kérdés kötelező szöveg.'
+            );
+
+            $this->validateStringField(
+                $validator,
+                $group['pair_answer'] ?? null,
+                "tasks.$index.pairing.pairing_groups.$gIndex.pair_answer",
+                'A válasz kötelező szöveg.'
+            );
+        }
+    }
+
+    private function validateGrouping($validator, $task, $index)
+    {
+        if (count($task['grouping']['groups'] ?? []) > 4) {
+            $validator->errors()->add(
+                "tasks.$index.grouping.groups",
+                'Maximum 3 csoportot alkothatsz.'
+            );
+
+            return;
+        }
+
+        if (empty($task['grouping']['groups'])) {
+            $validator->errors()->add(
+                "tasks.$index.grouping.groups",
+                'Legalább egy csoport megadása kötelező.'
+            );
+
+            return;
+        }
+
+        foreach ($task['grouping']['groups'] as $gIndex => $group) {
+               $this->validateStringField(
+                $validator,
+                $group['name'] ?? null,
+                "tasks.$index.grouping.groups.$gIndex.name",
+                "A név kötelező szöveg"
+            );
+
+
+            if (strlen($group['name']??null) > 30) {
                 $validator->errors()->add(
-                    "tasks.$index.pairing.pairing_groups.$gIndex.pair_question",
-                    'A kérdés megadása kötelező.'
+                    "tasks.$index.grouping.groups.$gIndex.name",
+                    'A megadott csoportnév túlhosszú.'
                 );
             }
 
-            if (empty($group['pair_answer'])) {
+            if (count($group['items']) > 4) {
                 $validator->errors()->add(
-                    "tasks.$index.pairing.pairing_groups.$gIndex.pair_answer",
-                    'A válasz megadása kötelező.'
+                    "tasks.$index.grouping.groups.$gIndex.items",
+                    'Maximum 5 elem lehet egy csoportban.'
                 );
             }
+            if (empty($group['items'])) {
+                $validator->errors()->add(
+                    "tasks.$index.grouping.groups.$gIndex.items",
+                    'Üres csoportot nem hozhatsz létre.'
+                );
+            }
+
+            // if (empty($group['pair_question'])) {
+            //     $validator->errors()->add(
+            //         "tasks.$index.pairing.pairing_groups.$gIndex.pair_question",
+            //         'A kérdés megadása kötelező.'
+            //     );
+            // }
+
+            // if (empty($group['pair_answer'])) {
+            //     $validator->errors()->add(
+            //         "tasks.$index.pairing.pairing_groups.$gIndex.pair_answer",
+            //         'A válasz megadása kötelező.'
+            //     );
+            // }
         }
     }
 
     private function validateTaskByType($validator, $task, $index)
     {
         return match ($task['task_type_id']) {
-            // 1 => $this->validateGrouping($validator, $task, $index),
+            1 => $this->validateGrouping($validator, $task, $index),
             2 => $this->validatePairing($validator, $task, $index),
             // 3 => $this->validateShortAnswer($validator, $task, $index),
             // 4 => $this->validateAssignment($validator, $task, $index),
@@ -124,5 +190,12 @@ class StoreWorksheetRequest extends FormRequest
             'message' => 'Validációs hiba történt.',
             'errors' => $validator->errors(),
         ], 422));
+    }
+
+    private function validateStringField($validator, $value, $path, $message)
+    {
+        if (! is_string($value) || trim($value) === '') {
+            $validator->errors()->add($path, $message);
+        }
     }
 }
